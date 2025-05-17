5a7b4cb57bd6956c6d5f480d20870cccf98f4836
muham <muhambriana.31@gmail.com>
2025-05-18 00:58:18 +0700

    feat: submission 07-real-world-scenario

--- final-assessment/optional-07-real-world-scenario/main.js
+++ final-assessment/optional-07-real-world-scenario/main.js
@@ -43,37 +43,37 @@ updateOrderStatus(orders[1].id, 'Selesai');
 
 console.log(orders);
 
-/**
- * Output yang diharapkan:
- * [
- *   {
- *     id: '_someUniqueId1',
- *     customerName: 'Alice',
- *     items: [
- *       { name: 'Nasi Goreng', price: 20000 },
- *       { name: 'Teh Manis', price: 5000 }
- *     ],
- *     totalPrice: 25000,
- *     status: 'Diproses'
- *   },
- *   {
- *     id: '_someUniqueId2',
- *     customerName: 'Bob',
- *     items: [
- *       { name: 'Mie Goreng', price: 15000 },
- *       { name: 'Kopi', price: 10000 }
- *     ],
- *     totalPrice: 25000,
- *     status: 'Selesai'
- *   }
- * ]
- */
+// /**
+//  * Output yang diharapkan:
+//  * [
+//  *   {
+//  *     id: '_someUniqueId1',
+//  *     customerName: 'Alice',
+//  *     items: [
+//  *       { name: 'Nasi Goreng', price: 20000 },
+//  *       { name: 'Teh Manis', price: 5000 }
+//  *     ],
+//  *     totalPrice: 25000,
+//  *     status: 'Diproses'
+//  *   },
+//  *   {
+//  *     id: '_someUniqueId2',
+//  *     customerName: 'Bob',
+//  *     items: [
+//  *       { name: 'Mie Goreng', price: 15000 },
+//  *       { name: 'Kopi', price: 10000 }
+//  *     ],
+//  *     totalPrice: 25000,
+//  *     status: 'Selesai'
+//  *   }
+//  * ]
+//  */
 
 console.log(calculateTotalRevenue());
-/**
- * Output yang diharapkan:
- * 25000
- */
+// /**
+//  * Output yang diharapkan:
+//  * 25000
+//  */
 
 
 deleteOrder(orders[0].id);
--- final-assessment/optional-07-real-world-scenario/orders.js
+++ final-assessment/optional-07-real-world-scenario/orders.js
@@ -3,20 +3,52 @@ function generateUniqueId() {
   return `_${Math.random().toString(36).slice(2, 9)}`;
 }
 
-
 // TODO: buatlah variabel yang menampung data orders
-let orders;
+const orders = [];
 
 // TODO: selesaikan fungsi addOrder
-function addOrder(customerName, items) {}
+function addOrder(customerName, items) {
+  const totalPrice = items.reduce((acc, item) => acc + item.price, 0);
+  const order = new Order(generateUniqueId(), customerName, items, totalPrice, 'Menunggu') ;
+
+  orders.push(order);
+}
 
 // TODO: selesaikan fungsi updateOrderStatus
-function updateOrderStatus(orderId, status) {}
+function updateOrderStatus(orderId, status) {
+  const order = orders.find(order => order.id === orderId);
+  
+  // If order not found (with parameter id). Dont do more
+  if (!order) return;
+
+  order.status = status;
+}
 
 // TODO: selesaikan fungsi calculateTotalRevenue dari order yang berstatus Selesai
-function calculateTotalRevenue() {}
+function calculateTotalRevenue() {
+  return orders
+    .filter((order) => order.status === 'Selesai')
+    .flatMap((order) => order.items)
+    .reduce((acc, item) => acc + item.price, 0);
+}
 
 // TODO: selesaikan fungsi deleteOrder
-function deleteOrder(id) {}
+function deleteOrder(id) {
+  const indexOrder = orders.findIndex(order => order.id === id);
+
+  if(indexOrder === -1) return;
+
+  orders.splice(indexOrder, 1);
+}
+
+class Order {
+  constructor(id, customerName, items, totalPrice, status) {
+    this.id = id;
+    this.customerName = customerName;
+    this.items = items;
+    this.totalPrice = totalPrice;
+    this.status = status;
+  }
+}
 
 export { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder };
